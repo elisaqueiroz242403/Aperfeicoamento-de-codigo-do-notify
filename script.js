f62d5cb06b714b8f1798120d891868ad2e858400
kare queiroz <kqueiroz812@gmail.com>
2019-07-25 22:01:25 -0300

    adicionar esquema dos bebes

---
+++ script.js
@@ -0,0 +1,11 @@
+let baby = prompt("Digite baby1 ou baby2");
+let baby1
+let baby2
+
+if(baby == baby1){
+    alert(prompt("É um principe, vem Heitor"));
+}
+
+else if(baby == baby2){
+    alert(prompt("Puts, esse não quis se revelar!"));
+}
